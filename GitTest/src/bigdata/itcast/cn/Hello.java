93a84be417887b670065c2f42d4498b44252cffd
Yang <yang@itcast.com>
2020-06-16 11:10:33 +0800

    version2

--- GitTest/src/bigdata/itcast/cn/Hello.java
+++ GitTest/src/bigdata/itcast/cn/Hello.java
@@ -2,6 +2,7 @@ package bigdata.itcast.cn;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello ! ");
+        System.out.println("World ! ");
     }
 }
